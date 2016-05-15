99efa249af891f8f1e34b3965b157a10286df75c
sebastien <sberidot@libertysurf.fr>
2016-05-15 03:38:08 +0200

    Now works. But expanded file seems to be in a different charset than uir one. awk does not manage to parse it correctly.

--- dev.sh
+++ dev.sh
@@ -4,7 +4,7 @@ function expand {
 	#echo "$@" > "/dev/stderr"
 	echo -n "$1 "
 	shift
-	for i; do
+	locales=C; for i; do
 		#echo "[$i]: " > /dev/stderr
 		echo {A..Z}{A..Z} | awk '{ for(i=1; i <= NF; i++) print $i }' | grep -E "$i"
 	done | awk '{ printf("%s", $1) } END { printf("\n") }'
