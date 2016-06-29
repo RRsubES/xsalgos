b44e1354261c28c41a817286fa317410278bbde4
sebastien <sberidot@libertysurf.fr>
2016-06-29 22:50:35 +0200

    Fixing octal problem converting two fields in a single value. DATE and TIME are now DATE.TIME float number. More compact and less error prone. Fixing the whole pipeline accordingly. Still easy with uniq, because same length -w 9 uniq parms. And still easy to read for terminating earlier than expected the pipeline.

--- gen.scripts.sh
+++ gen.scripts.sh
@@ -8,15 +8,34 @@ function usage {
 		echo -e "${RED}[E]${NORMAL}: $1" > "/dev/stderr"
 	fi
 cat << EOF > "/dev/stderr"
-Syntax: $(basename $0) DATE_YYYYMMDD TIME_HHMM < XSALGOS_FILE
-For any new group, amend list.rgrp file. No pipe allowed, only redirected file.
+Syntax: $(basename $0) DATE TIME [-q|--quiet] [-f|--keep-filter] < XSALGOS_FILE
+
+DATE and TIME formats:
+		DATE YYYYMMDD
+		TIME HHMM
+
+-f		: keep the script used to extract information. Filename is specified.
+		  Syntax to use it:
+		  e.g.: ./filter.12345.awk < XSALGOS_FILE
+-h		: displays help
+-q		: quiet mode (unset by default)
+
+For any new group, amend list.rgrp file, if not found, simple sort is applied.
+The order of the parameters does not matter anymore.
 
 e.g.: $(basename $0) 20160504 1905 < xslgs.04-05
 
 e.g.: cat xslgs.01-05 xslgs.02-05 xslgs.03-05 > xslgs 
-      $(basename $0) 20160503 0005 < xslgs
+      $(basename $0) -q -f 20160503 0005 < xslgs
 EOF
 	exit 1
+}
+
+function echo_if {
+	if [ ${OPTION_QUIET} -ne 0 ]; then
+		return
+	fi
+	echo "$@" > /dev/stderr
 }
 
 [ $# -eq 0 ] && usage "no parameter detected"
@@ -24,11 +43,13 @@ EOF
 
 TIME_REGEX="([01][0-9]|2[0-3])[0-5][0-9]"
 
+# stdin date pattern, may contain blanks
 DAY_IN_REGEX="( [1-9]|[12][0-9]|3[01])"
 MONTH_IN_REGEX="( [1-9]|1[012])"
-YEAR_IN_REGEX="([0-9]{4})"
+YEAR_IN_REGEX="(20[0-9]{2})"
 DATE_IN_REGEX="(${DAY_IN_REGEX}\/${MONTH_IN_REGEX}\/${YEAR_IN_REGEX})"
 
+# in generated awk scripts, no blanks, only figures
 DAY_REGEX="${DAY_IN_REGEX// /0}"
 MONTH_REGEX="${MONTH_IN_REGEX// /0}"
 YEAR_REGEX="${YEAR_IN_REGEX}"
@@ -45,48 +66,74 @@ GLIST_REGEX="(${GSECT_REGEX} +)+"
 
 DEFCF_REGEX="${POSI_REGEX} += (FERMEE|${GLIST_REGEX})"
 
+OPTION_QUIET=0
+OPTION_ERASE_FILTER=1
+
 #-v TIME: checks if TIME variable has been set
-#FIXME: bc stuff because 0500 can be evaluated otherwise as octal, in awk as well...
-[[ $1 =~ ^${DATE_REGEX}$ ]] && { DATE="$1"; shift; }
-[[ $1 =~ ^${TIME_REGEX}$ ]] && { TIME="$1"; shift; }
+
 # -t 0: check if stdin is connected to a terminal
 # -p 0: check if stdin is connected via a pipe
 # if both fail, it means it was redirected via a "< FILE"
 #echo "pipe? $([[ -p 0 ]]; echo $?)"
 #echo "terminal? $([[ -t 0 ]]; echo $?)"
-[[ -v DATE ]] && [[ -v TIME ]] && [[ -p 0 || -t 0 ]] && [[ -f "$1" ]] && { exec < "$1"; shift; }
 
 while (($# > 0)); do
-	case "$1" in
-	-h|--help)
-		usage
-		;;
-	*)
-		usage "$1 is of type unknown"
-		;;
-	esac
+	if [[ $1 =~ ^${DATE_REGEX}$ ]]; then
+		DATE="$1"
+	elif [[ $1 =~ ^${TIME_REGEX}$ ]]; then
+		TIME="$1"
+	else
+		case "$1" in
+		-f|--keep-filter)
+			OPTION_ERASE_FILTER=0
+			;;
+		-h|--help)
+			usage
+			;;
+		-q|--quiet)
+			OPTION_QUIET=1
+			;;
+		*)
+			if [[ -t 0 || -p 0 ]] && [ -f "$1" ]; then
+				exec < "$1"
+			else
+				usage "$1 is of type unknown"
+			fi
+			;;
+		esac
+	fi
 	shift
 done
 
-#echo "TIME? $([[ -v TIME ]]; echo $?)"
 [[ ! -v DATE ]] && usage "DATE_YYYYMMDD not defined"
 [[ ! -v TIME ]] && usage "TIME_HHMM not defined"
 #[[ -p 0 || -t 0 ]] && usage "No valid detected data to process"
+[[ ${OPTION_QUIET} -eq 0 ]] && { echo_if "DATE=${DATE} TIME=${TIME}" ; }
 
 echo "#!/usr/bin/awk -f
-function display(d,h,l) {
+function display(d,h,p,l,		ary,i) {
 	# delete military sectors
 	gsub(/${MSECT_REGEX}/, \"\", l)
-	# delete extra spaces
-	gsub(/ +/, \" \", l)
-	gsub(/^ +/,\"\", l)
-	if (!length(d))
-		print l, \" : date not found, left out\" > \"/dev/stderr\"
+	if (!length(d)) {
+		print \"At line\", NR ,\"date not available yet, check header, exiting\" > \"/dev/stderr\"
+		exit(1)	
+	}
+	split(l,ary)	
+	asort(ary)
+	l=\"\"
+	for(i=1; i<=length(ary); i++)
+		l=l ary[i] \" \"
 	if (length(l) > 0 && length(h) > 0) 
-		print d, h, l
+		print d\".\"h, p, \"=\", l
 }
 
-/^\*\*\* JOURNEE DU ${DATE_IN_REGEX} CALCULATEUR [0-9] \*\*\*/ {
+/^\*\*\* JOURNEE DU ${DATE_IN_REGEX} CALCULATEUR [0-9] \*\*\*$/ {
+	# expression may be found more than once (multiple concatenated
+	# files), * special case *
+	if (length(DATE) > 0) {
+		display(DATE,HOUR,POSI,LINE)
+		DATE=HOUR=LINE=\"\"
+	}
 	DAY=substr(\$0,16,2)
 	MONTH=substr(\$0,19,2)
 	YEAR=substr(\$0,22,4)
@@ -94,69 +141,67 @@ function display(d,h,l) {
 	gsub(/ /, \"0\", MONTH)
 	gsub(/ /, \"0\", YEAR)
 	DATE=YEAR \"\" MONTH \"\" DAY
+	next
 }
 
 /^0\*\*\* ${TIME_REGEX} STPV-Loc OPP ${DEFCF_REGEX}\$/ {
-	display(DATE,HOUR,LINE)
+	display(DATE,HOUR,POSI,LINE)
 	HOUR=\$2
 	LINE=\"\"
 	if (\$7 == \"FERMEE\")
 		next
-	\$1=\$2=\$3=\$4=\"\"
+	POSI=\$5
+	\$1=\$2=\$3=\$4=\$5=\$6=\"\"
 	LINE=\$0
+	next
 }
 
 /^ +${GLIST_REGEX}\$/ {
 	LINE=LINE\$0
+	next
 }
 
 /^ +${DEFCF_REGEX}\$/ {
-	display(DATE,HOUR,LINE)
+	display(DATE,HOUR,POSI,LINE)
 	LINE=\"\"
 	if (\$3 == \"FERMEE\")
 		next
+	POSI=\$1
+	\$1=\$2=\"\"
 	LINE=\$0
+	next
 }
 
 END {
-	display(DATE,HOUR,LINE)
+	display(DATE,HOUR,POSI,LINE)
 }" > filter.$$.awk
 chmod +x filter.$$.awk
 
 echo "#!/usr/bin/awk -f
 
-function is_before(d,h) {
-	if (d > $(echo ${DATE} | bc))
-		return 0
-	if (d < $(echo ${DATE} | bc))
-		return 1
-	if (h <= $(echo ${TIME} | bc))
+function is_before(tc) {
+	if (tc <= ${DATE}.${TIME})
 		return 1
 	return 0
 }
 
 # military sectors already left out...
-/^${DATE_REGEX} ${TIME_REGEX} ${POSI_REGEX} = ${CLIST_REGEX}\$/ {
-	DATE=\$1
-	HOUR=\$2
-	if (is_before(DATE,HOUR)) {
-		for(i = 5; i <= NF; i++)
-			db[\$i]=\$3
+/^${DATE_REGEX}\.${TIME_REGEX} ${POSI_REGEX} = ${CLIST_REGEX}\$/ {
+	TIME_CHECK=\$1
+	if (is_before(TIME_CHECK)) {
+		for(i = 4; i <= NF; i++)
+			db[\$i]=\$2
 	} else
 		exit 0
 }
 
 END {
-	if (is_before(DATE,HOUR)) {
+	if (is_before(TIME_CHECK)) {
 		printf(\"${DATE} not found, exiting\\n\") > \"/dev/stderr\"
 		exit 1
 	}
-	layer=\"NDSHIU\"
-	sect=\"I NGA JVKW MQZX\"
-	for(s in db) {
-		#printf(\"%s %d %s = %s\n\", index(sect,substr(s,1,1)), index(layer,substr(s,2,1)), s, db[s])
+	for(s in db)
 		printf(\"%s = %s\n\", s, db[s])
-	}
 }" > read.along.$$.awk
 chmod +x read.along.$$.awk
 
@@ -168,7 +213,6 @@ echo "#!/usr/bin/awk -f
 END {
 	layer=\"NDSHIU\"
 	sect=\"I NGA JVKW MQZX\"
-	# printf(\"Statut à %s:\n\", ${TIME}) > \"/dev/stderr\"
 	for(i = 1; i <= length(sect); i++) {
 		if (substr(sect, i, 1) == \" \")
 			print \"\"
@@ -184,11 +228,12 @@ END {
 chmod +x sort.$$.awk
 
 if [ -e list.rgrp ]; then
-	./filter.$$.awk "$@" | ./read.along.$$.awk | awk -f expand.and.rename.awk "PASS=1" list.rgrp "PASS=2" /dev/stdin | sort
+	sed -E "s/\x03//" | ./filter.$$.awk "$@" | ./read.along.$$.awk | awk -f expand.and.rename.awk "PASS=1" list.rgrp "PASS=2" /dev/stdin | sort
 else
-	echo "${RED}[E]${NORMAL} list.rgrp not found, unable to shorten expressions" > /dev/stderr
-	./filter.$$.awk "$@" | ./read.along.$$.awk | ./sort.$$.awk
+	echo_if "${RED}[E]${NORMAL} list.rgrp not found, unable to shorten expressions"
+	sed -E "s/\x03//" | ./filter.$$.awk "$@" | ./read.along.$$.awk | ./sort.$$.awk
 fi
 
-rm -f filter.$$.awk read.along.$$.awk sort.$$.awk 2>/dev/null
+[[ ${OPTION_ERASE_FILTER} -ne 0 ]] && rm -f filter.$$.awk 2>/dev/null || echo_if "${RED}Script filename:${NORMAL} filter.$$.awk"
+rm -f read.along.$$.awk sort.$$.awk 2>/dev/null
 
--- pickfile.sh
+++ pickfile.sh
@@ -23,47 +23,31 @@ HOUR_REGEX="([01][0-9]|2[0-3])"
 MINUTE_REGEX="([0-5][0-9])"
 TIME_REGEX="(${HOUR_REGEX}${MINUTE_REGEX})"
 
+[[ $# -eq 0 ]] || [[ $1 == "--help" ]] && usage
 [[ $1 =~ ^${DATE_REGEX}$ ]] && { DATE="$1"; shift; }
 [[ $1 =~ ^${TIME_REGEX}$ ]] && { TIME="$1"; shift; }
 
 YEAR=${DATE:0:4}
+[[ ! ${YEAR} =~ ^${YEAR_REGEX}$ ]] && usage "${YEAR} is not detected as being
+a valid year pattern"
 
-#JAN_REGEX="((01)_?([Jj]an(vier)?))"
-#FEB_REGEX="((02)_?([Ff][ée]v(rier)?))"
-#MAR_REGEX="((03)_?([Mm]ars))"
-#APR_REGEX="((04)_?([Aa]vril))"
-#MAY_REGEX="((05)_?([Mm]ai))"
-#JUN_REGEX="((06)_?([Jj]uin))"
-#JUL_REGEX="((07)_?([Jj]uillet))"
-#AUG_REGEX="((08)_?([Aa]out))"
-#SEP_REGEX="((09)_?([Ss]ep(tembre)?))"
-#OCT_REGEX="((10)_?([Oo]ct(obre)?))"
-#NOV_REGEX="((11)_?([Nn]ov(embre)?))"
-#DEC_REGEX="((12)_?([Dd][ée]c(embre)?))"
-#
-#NUMMONTH_REGEX="(${JAN_REGEX}|${FEB_REGEX}|${MAR_REGEX}|${APR_REGEX}|${MAY_REGEX}|${JUN_REGEX}|${JUL_REGEX}|${AUG_REGEX}|${SEP_REGEX}|${OCT_REGEX}|${NOV_REGEX}|${DEC_REGEX})"
-#FIND_REGEX=".*/${YEAR_REGEX}/${NUMMONTH_REGEX}"
+# must be changed in the * big * for/find
+# a[6] and a[7] refers to year and month
+FIND_DIR="./_XSALGOS/"
 
-# may need to change it and expressions such as a[6], a[7]...
-FIND_DIR="./_XSALGOS"
+#echo "date: ${DATE}" > /dev/stderr
 
-echo "date: ${DATE}" > /dev/stderr
 files=()
 for f in $(find "${FIND_DIR}" -mindepth 2 -type f | grep -E "[xX][sS][lL][gG][sS]_[0-9]{2}\.[0-9]{2}(\.[tT][xX][tT])?$" | grep -v -E "([xX][sS][cC][dD][sS]2|STATUCE)" | awk '{ n=split($0,a,"/"); printf("%s%s%s %s\n", a[3], substr(a[4],0,2), substr(a[n],7,2), $0); }' | sort | uniq -w 8 | awk "{ print ; if (\$1 == ${DATE}) exit(0); }" | tail -n 2 | sed -E "s/([0-9]{8} )(.*)/\2/"); do
 	files+=( "$f" )
 	echo "${f#${FIND_DIR}}" > /dev/stderr
 done
-#echo "${files[@]}" > /dev/stderr
-#printf '%s\n' "${files[@]}" > /dev/stderr
-#echo "${files[@]//${FIND_DIR}/}" | tr " " "\n" > /dev/stderr
+
 read -p "Proceeding with those files ? [Y] " choice
 if [[ $choice =~ ^[nN] ]]; then
 	exit 0
 fi
 
 #cat "${files[@]}" | sed -E "s/\x03//" > tmp.$$.txt 
-cat "${files[@]}" | sed -E "s/\x03//" | ./gen.scripts.sh ${DATE} ${TIME}
-#echo "tmp.$$.txt created" > /dev/stderr
+cat "${files[@]}" | ./gen.scripts.sh ${DATE} ${TIME}
 
-
-#find ../penndolog/sub_CV/Echanges_CV-ES/_XSALGOS -mindepth 2 -type f| grep -E "[xX][sS][lL][gG][sS]_[0-9]{2}.[0-9]{2}(\.[tT][xX][tT])?$" | grep -v -E "([xX][sS][cC][dD][sS]2|STATUCE)" | awk '{ n=split($0,a,"/"); printf("%s%s%s %s\n", a[6],substr(a[7],0,2),substr(a[n],7,2), $0); }' | sort | uniq -w 8 | awk "{ print ; if (\$1 == 20160607) exit(0); }"
