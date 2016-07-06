8a8734abe7094d3c25e0ca5526328e7cea4c2a09
sebastien <sberidot@libertysurf.fr>
2016-07-06 15:35:45 +0200

    fetch.sh: -a|--auto option added, auto mode.

--- fetch.sh
+++ fetch.sh
@@ -4,6 +4,7 @@
 
 #NO BLANK ALLOWED IN FILENAMES
 OPTION_BEFORE=1
+OPTION_AUTO=1
 declare -r BEFORE_REGEX="([0-9]+)"
 
 function usage {
@@ -11,19 +12,23 @@ function usage {
 		err "$1"
 	fi
 cat << EOF > "/dev/stderr"
-Syntax: $(basename $0) [-b|--before n] DATE_YYYYMMDD 
+Syntax: $(basename $0) [-a|--auto] [-b|--before n] DATE_YYYYMMDD 
 
+-a		: skip questions, auto mode.
 -b n		: how many days are kept before the date
 		(1 by default).
 
 e.g.: $(basename $0) 20160504
-e.g.: $(basename $0) -b 3 20160504
+e.g.: $(basename $0) -a -b 3 20160504
 EOF
 	exit 1
 }
 
 while (($# > 0)); do
 	case "$1" in
+	-a|--auto)
+		OPTION_AUTO=0
+		;;
 	-b|--before)
 		OPTION_BEFORE=$2
 		shift
@@ -54,27 +59,28 @@ if [ ${#files[@]} -eq 0 ]; then
 	info "No file found with ${DATE}"
 	exit 0
 fi
-read -p "Proceeding with those files ? [Y] " choice > /dev/stderr
-if [[ $choice =~ ^[nN] ]]; then
-	exit 0
-else
-	headers_missing=0
-	for f in ${files[@]}; do
-		check_header "$f"
-		[ $? -ne 0 ] && { (( headers_missing++ )); err ">> $f"; }
-		list="${list} $f"
-	done
-	if [ ${headers_missing} -gt 0 ]; then
-		err "${headers_missing} invalid header(s) found, lack of:"
-		err "${HEADER_REGEX//\\/}"
-		err "Fix them first."
-		exit 1
-	fi
-	# problem if prev file does not end up with LF
-	# need to add it with echo
-	#cat "${files[@]}" | sed -E "s/${ETX_CHAR}//"
-	for f in ${files[@]}; do
-		cat "$f"; echo ""
-	done | sed -E "s/${ETX_CHAR}//"
+
+if [ ${OPTION_AUTO} -eq 1 ]; then
+	read -p "Proceeding with those files ? [Y] " choice > /dev/stderr
+	[ $choice =~ ^[nN] ] && exit 0
 fi
+
+headers_missing=0
+for f in ${files[@]}; do
+	check_header "$f"
+	[ $? -ne 0 ] && { (( headers_missing++ )); err ">> $f"; }
+	list="${list} $f"
+done
+if [ ${headers_missing} -gt 0 ]; then
+	err "${headers_missing} invalid header(s) found, lack of:"
+	err "${HEADER_REGEX//\\/}"
+	err "Fix them first."
+	exit 1
+fi
+# problem if prev file does not end up with LF
+# need to add it with echo
+#cat "${files[@]}" | sed -E "s/${ETX_CHAR}//"
+for f in ${files[@]}; do
+	cat "$f"; echo ""
+done | sed -E "s/${ETX_CHAR}//"
 
